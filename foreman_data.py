4b03f8155a6924ae8d1ee909988dfe2593a4df6d
barros003 <barros003@hotmail.com>
2022-11-01 18:01:35 -0300

    dsds

--- foreman_data.py
+++ foreman_data.py
@@ -13,12 +13,12 @@ def get_json(url):
     r = requests.get(url, auth=(USERNAME, PASSWORD), verify=SSL_VERIFY)
     return r.json()
 
-def get_errata_count(url):
+def get_errata_count_by_host(url):
     resultsjson = get_json(url)
     if resultsjson:
          data = resultsjson["content_facet_attributes"]["errata_counts"]
          for key, value in data.items():
-            print(key, value)         
+            print(key, value)  
          
          #securitypatch = resultsjson["content_facet_attributes"]["errata_counts"]["security"]
          #bugfixpatch = resultsjson["content_facet_attributes"]["errata_counts"]["bugfix"]
@@ -26,9 +26,19 @@ def get_errata_count(url):
          #total = resultsjson["content_facet_attributes"]["errata_counts"]["total"]
          #print("totalpatch: " + str(total))
 
+def get_errata_count_all(url):
+    resultsjson = get_json(url)
+    if resultsjson:
+        data = resultsjson["results"]["content_facet_attributes"]
+        for host in data.items():
+            print(str(host["cert_name"]))
+            print(str(host["errata_counts"]))
+
+
 def main():
     host = "oracle7"    
-    get_errata_count(SAT_API + 'hosts/' + host)
+    #get_errata_count_by_host(SAT_API + 'hosts/' + host)
+    get_errata_count_all(SAT_API + 'hosts/')
 
 if __name__ == "__main__":
     main()
