1c54b2532e5dc0a6a6938059a34f259403500a13
barros003 <barros003@hotmail.com>
2022-11-04 19:45:42 -0300

    add logging

--- foreman_data.py
+++ foreman_data.py
@@ -1,54 +1,94 @@
 import json
 import sys
 import requests
+import logging
+
 
 
 SAT = "192.168.213.98"
+#SAT = ""
 SAT_API = f"https://{SAT}/api/"
 USERNAME = "admin"
 PASSWORD = "qjsqulVPKyvDGRwVPLIaNQ"
+#USERNAME = ""
+#PASSWORD = ""
 SSL_VERIFY = False
+ORG_ID = 3
 
-def get_json(url):
-    r = requests.get(url, auth=(USERNAME, PASSWORD), verify=SSL_VERIFY)
-    return r.json()
 
-def get_errata_count_by_host(url):
-    resultsjson = get_json(url)
+def main_erratum():
+   logging.basicConfig(level=logging.INFO, filename="main.log", format="%(asctime)s - %(levelname)s - %(message)s")
+   try:
+      
+      url = SAT_API + f"v2/hosts?search=name%20!~%20virt-who*%20and%20organization_id={ORG_ID}&per_page=550"
+      logging.info(f"Calling endpoint:{url}")
+      r = requests.get(url, auth=(USERNAME, PASSWORD), verify=SSL_VERIFY)
+      r.raise_for_status()
+      errata_count  = get_errata_count_all(r.json())
+      errata_byhost = get_errata_count_by_host(r.json())  
+   except requests.exceptions.HTTPError as err:
+      logging.error("Cannot access the endpoint:" + url + err) 
+   except requests.exceptions.Timeout as errt:
+      logging.error("Timeout accessing:" + url + errt)
+       
+  
+   logging.info(f"Endpoint ok")
+   return errata_count, errata_byhost
+
+def get_errata_count_by_host(json_return):
+    
+    resultsjson = json_return
+  
     if resultsjson:
-         data = resultsjson["content_facet_attributes"]["errata_counts"]
-         for key, value in data.items():
-            print(key, value)        
+         data = resultsjson["results"]
+         erratum = dict()
+         count = 1
+         for entry in data:
+           if "content_facet_attributes" in entry:             
+             hostname = str(entry["certname"])
+
+             security_count = entry["content_facet_attributes"]["errata_counts"]["security"]
+             erratum.update({f"sec{count}" : {"host": hostname, "errata_type": "security", "errata_count": security_count }})              
+             
+             bugfix_count = entry["content_facet_attributes"]["errata_counts"]["bugfix"]               
+             erratum.update({f"bug{count}": {"host": hostname, "errata_type": "bugfix", "errata_count": bugfix_count }})   
+             
+             enhancement_count = entry["content_facet_attributes"]["errata_counts"]["enhancement"]                
+             erratum.update({f"enha{count}": {"host": hostname, "errata_type": "enhancement", "errata_count": enhancement_count }})
+              
+             count += 1
+
+    return erratum  
 
 
-def get_errata_count_all():
-    url = SAT_API + 'hosts/'
-    resultsjson = get_json(url)
+def get_errata_count_all(json_return):
+ 
+    resultsjson = json_return
     security_count = 0
     bugfix_count = 0
     enhancement_count = 0
     if resultsjson:
         data = resultsjson["results"]
-        total = resultsjson["total"]
+        total = resultsjson["subtotal"]
         for entry in data:
            if "content_facet_attributes" in entry:
               if entry["content_facet_attributes"]["errata_counts"]["security"] > 0:
                 security_count += 1
-                #print(entry["certname"],"Security",entry["content_facet_attributes"]["errata_counts"]["security"])
+                
               if entry["content_facet_attributes"]["errata_counts"]["bugfix"] > 0:
                 bugfix_count += 1
-                #print(entry["certname"],"Bugfix",entry["content_facet_attributes"]["errata_counts"]["bugfix"])                
+                            
               if entry["content_facet_attributes"]["errata_counts"]["enhancement"] > 0:
-                enhancement_count += 1
-                #print(entry["certname"],"enhancement",entry["content_facet_attributes"]["errata_counts"]["enhancement"])
+                enhancement_count += 1            
 
     return security_count,  bugfix_count,  enhancement_count, total
-    #print (security_count, bugfix_count, enhancement_count)
+ 
 
-def main():
-    host = "oracle7"    
-    #get_errata_count_by_host(SAT_API + 'hosts/' + host)
-    #get_errata_count_all()
+#def main():
+    
+    #get_errata_count_by_host("security")
+    #testedict()
+    
 
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+#    main()
--- foremangraf.py
+++ foremangraf.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from foreman_data import *
+import time
+import logging
 
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
@@ -8,25 +10,44 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 token = "_v-lv3tBs3LEoxNlD8p1pYYhEfx8IVJqO4-s_R3Ti7-1j9FFFW2NN6owDH4hb6_BytJ0huQY2SQ6b4TTOqzxgA=="
 org = "myown"
 bucket = "satellite"
+sync_time = 600
 
 client = InfluxDBClient(url="http://192.168.213.48:8086", token=token)
 
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
-res = get_errata_count_all()
-security    = str(res[0])
-bugfix      = str(res[1])
-enhancement = str(res[2])
-total       = str(res[3])
+def write_to_influxdb():
+ while True:
+    logging.basicConfig(level=logging.INFO, filename="main.log", format="%(asctime)s - %(levelname)s - %(message)s")
+    logging.info("Starting new sync sequence")
+    res = main_erratum()
+    security    = str(res[0][0])
+    bugfix      = str(res[0][1])
+    enhancement = str(res[0][2])
+    total       = str(res[0][3])
 
-data = f"patchs,type=security totalmachines={security}"
-write_api.write(bucket, org, data)
+    data = f"patchs,type=security totalmachines={security}"
+    write_api.write(bucket, org, data) 
 
-data = f"patchs,type=bugfix totalmachines={bugfix}"
-write_api.write(bucket, org, data)
+    data = f"patchs,type=bugfix totalmachines={bugfix}"
+    write_api.write(bucket, org, data)
+    
+    data = f"patchs,type=enhancement totalmachines={enhancement}"
+    write_api.write(bucket, org, data)
+    
+    data = f"patchs,type=total totalmachines={total}"
+    write_api.write(bucket, org, data)
+    
 
-data = f"patchs,type=enhancement totalmachines={enhancement}"
-write_api.write(bucket, org, data)
-
-data = f"patchs,type=total totalmachines={total}"
-write_api.write(bucket, org, data)
+    for key in res[1]:
+                
+        hostname     = res[1][key]["host"]
+        errata_type  = res[1][key]["errata_type"]
+        errata_count = res[1][key]["errata_count"]              
+        data = f"patchs,host={hostname} {errata_type}={errata_count}"
+        write_api.write(bucket, org, data)
+    timeinminute = sync_time//60    
+    logging.info(f"Next sync in: {timeinminute} minutes")
+    time.sleep(sync_time)
+        
+write_to_influxdb()
